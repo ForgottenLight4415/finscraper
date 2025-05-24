2ca288792f01683c2418c22c50e87af748f10a6e
ForgottenLight4415 <crv4415@gmail.com>
2025-05-24 00:05:18 -0400

    feat: updated tool to find news by ticker

--- scraper_tool/tool.py
+++ scraper_tool/tool.py
@@ -1,5 +1,6 @@
 from langchain_core.tools import Tool
 from .fetcher import fetch_stock_data_yf
+from .news_fetcher import fetch_finance_news
 
 def get_stock_info_tool(ticker: str) -> str:
     data = fetch_stock_data_yf(ticker)
@@ -12,8 +13,21 @@ def get_stock_info_tool(ticker: str) -> str:
         f"Net Income: {data['net_income']}"
     )
 
+def get_stock_news_tool(ticker: str) -> str:
+    data = fetch_finance_news(ticker, scrolls=3, limit=10)
+    content = ""
+    for article in data:
+        content += article["content"]
+    return content
+
 stock_data_tool = Tool(
     name="StockDataFetcher",
     func=get_stock_info_tool,
     description="Use this tool to fetch financial metrics of a public company by ticker symbol. Example: AAPL, MSFT, TSLA"
+)
+
+stock_news_tool = Tool(
+    name="StockNewsFetcher",
+    func=get_stock_news_tool,
+    description="Use this tool to fetch financial news of a public company by ticker symbol. Example: AAPL, MSFT, TSLA"
 )
